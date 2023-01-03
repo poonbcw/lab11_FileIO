c9162ff07ec2246b3cc4c5582b8ebf8bd8d296da
poon <poon15112@gmail.com>
2023-01-03 16:27:30 +0700

    lap11_3 completed

--- lab11_3.cpp
+++ lab11_3.cpp
@@ -1,3 +1,29 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include <iostream>
+#include <iomanip>
+#include<fstream>
+#include<cstdlib>
+#include<string>
+#include<cmath>
+using namespace std;
+
+int main(){
+    int i = 0;
+    double sum = 0;
+    double sum2 = 0;
+    double data[83] ={};
+    ifstream source("score.txt");
+    string textline;
+    while(getline(source,textline)){
+        data[i] = atof(textline.c_str());
+        sum += data[i];
+        sum2 += pow(data[i],2);
+        i++;
+    }
+    cout << "Number of data = " << i << "\n";
+    cout << setprecision(3);
+    cout << "Mean = " << sum/i << "\n";
+    cout << "Standard deviation = " << sqrt((sum2/i)-pow(sum/i,2));
+    
+    source.close();
+    return 0;
+}
